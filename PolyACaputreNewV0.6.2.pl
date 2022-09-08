0b0e649825320a3debd5a0afb385bd5220a32c01
Andreas GIsel <gisels4@gmail.com>
2022-09-08 11:56:08 +0200

    Update PolyACaputreNewV0.6.2.pl

--- PolyACaputreNewV0.6.2.pl
+++ PolyACaputreNewV0.6.2.pl
@@ -120,7 +120,12 @@ while(<IN>)
 	{
 		$end = $end[1];
 	}
-	print "$end[2] - $intron_ev - $end - $end[0] - $end[1]\n";
+	
+	if($end[2]>0)
+	{
+		print "$end[2] - $intron_ev - $end - $end[0] - $end[1]\n";
+	}
+	
 	next if($end[3] == 4);								# hit fragment shorter than min_match
 	
 	if($data[3] >= $Clu_start)						# if new hit position is bigger than last hit we are still on the same chromosome
